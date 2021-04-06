afb4b6ab05f9159d928753264c9fff5d5c977eed
abc <abc@sohu.com>
2021-04-06 10:45:33 +0800

    update

---
+++ src/main/java/com/atguigu/bean/OrderEvent.java
@@ -0,0 +1,19 @@
+package com.atguigu.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author lizhenchao@atguigu.cn
+ * @Date 2021/4/6 10:23
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEvent {
+    private Long orderId;
+    private String eventType;
+    private String txId;
+    private Long eventTime;
+}
---
+++ src/main/java/com/atguigu/bean/TxEvent.java
@@ -0,0 +1,18 @@
+package com.atguigu.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author lizhenchao@atguigu.cn
+ * @Date 2021/4/6 10:23
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TxEvent {
+    private String txId;
+    private String payChannel;
+    private Long eventTime;
+}
