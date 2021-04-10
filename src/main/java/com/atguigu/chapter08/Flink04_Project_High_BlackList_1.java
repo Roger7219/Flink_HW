fe632fd81c82d5a6577b9f1fa53eb3f08623f2ef
abc <abc@sohu.com>
2021-04-10 10:19:44 +0800

    update

--- src/main/java/com/atguigu/chapter08/Flink04_Project_High_BlackList_1.java
+++ src/main/java/com/atguigu/chapter08/Flink04_Project_High_BlackList_1.java
@@ -16,16 +16,15 @@ import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.OutputTag;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneOffset;
+import java.util.Date;
 
 /**
  * @Author lizhenchao@atguigu.cn
  * @Date 2021/4/10 9:06
  */
-public class Flink04_Project_High_BlackList {
+public class Flink04_Project_High_BlackList_1 {
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         conf.setInteger("rest.port", 20000);
@@ -41,12 +40,12 @@ public class Flink04_Project_High_BlackList {
                     return element.getTimestamp() * 1000L;
                 }
             });
-    
+        
         OutputTag<String> blackListOutputTag = new OutputTag<String>("blackList", Types.STRING) {};
-    
+        
         SingleOutputStreamOperator<String> result = env
-//            .readTextFile("input/AdClickLog.csv")
-            .socketTextStream("hadoop162", 9999)
+            .readTextFile("input/AdClickLog.csv")
+            //            .socketTextStream("hadoop162", 9999)
             .map(line -> {
                 String[] datas = line.split(",");
                 return new AdsClickLog(Long.valueOf(datas[0]),
@@ -59,7 +58,8 @@ public class Flink04_Project_High_BlackList {
             // 计算每个用户对每个广告的点击量
             .keyBy(ads -> ads.getUserId() + "_" + ads.getAdsId())
             .process(new KeyedProcessFunction<String, AdsClickLog, String>() {
-    
+                
+                private ValueState<String> dateState;
                 private ValueState<Boolean> warnState;
                 private ReducingState<Long> countState;
                 
@@ -76,9 +76,10 @@ public class Flink04_Project_High_BlackList {
                             },
                             Long.class
                         ));
-    
+                    
                     warnState = getRuntimeContext().getState(new ValueStateDescriptor<Boolean>("warnState", Boolean.class));
                     
+                    dateState = getRuntimeContext().getState(new ValueStateDescriptor<String>("dateState", String.class));
                     
                 }
                 
@@ -86,18 +87,19 @@ public class Flink04_Project_High_BlackList {
                 public void processElement(AdsClickLog log,
                                            Context ctx,
                                            Collector<String> out) throws Exception {
-                    // 每个用户每个广告第一条数据, 注册定时器, 到明天的0:0:0去触发: 清空状态
-                    // 如果countState的值是null, 则表示这是第一条
-                    if (countState.get() == null) {
-                        // 如何找到明天的0:0:0
-                        long now = ctx.timestamp() / 1000;
-                        LocalDateTime todayTime = LocalDateTime.ofEpochSecond(now, 0, ZoneOffset.ofHours(8));
-                        LocalDateTime tomorrowTime = LocalDateTime.of(todayTime.toLocalDate().plusDays(1), LocalTime.of(0, 0, 0));
-    
-                        ctx.timerService().registerEventTimeTimer(tomorrowTime.toEpochSecond(ZoneOffset.ofHours(8)) * 1000);
+                    
+                    String today = new SimpleDateFormat("yyyy-MM-dd").format(new Date(log.getTimestamp() * 1000));
+                    if (!today.equalsIgnoreCase(dateState.value())) {
+                        countState.clear();
+                        dateState.clear();
+                        warnState.clear();
                     }
                     
-                    
+                    // 如果countState的值是null, 则表示这是第一条
+                    if (countState.get() == null) {
+                        // 把年月日存储到状态中
+                        dateState.update(new SimpleDateFormat("yyyy-MM-dd").format(new Date(log.getTimestamp() * 1000)));
+                    }
                     
                     if (warnState.value() == null) {
                         countState.add(1L);
@@ -110,7 +112,7 @@ public class Flink04_Project_High_BlackList {
                                     " 的点击量是: " + countState.get();
                             ctx.output(blackListOutputTag, msg);
                             warnState.update(true);
-                        }else{
+                        } else {
                             String msg =
                                 "用户: " +
                                     log.getUserId() +
@@ -120,19 +122,13 @@ public class Flink04_Project_High_BlackList {
                         }
                     }
                 }
-    
-                @Override
-                public void onTimer(long timestamp, OnTimerContext ctx, Collector<String> out) throws Exception {
-                    System.out.println("onTimer....");
-                    warnState.clear();
-                    countState.clear();
-                }
+                
             });
         
         result.print("正常用户");
         
         result.getSideOutput(blackListOutputTag).print("blackList");
-    
+        
         env.execute();
         
     }
